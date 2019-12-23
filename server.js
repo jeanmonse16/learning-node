95e891abc0dedb24137df7e8e0f678f71564d85e
jeanpier <jeanpier.dev@gmail.com>
2019-12-23 15:21:24 -0400

    creciendo en node

--- server.js
+++ server.js
@@ -3,10 +3,19 @@ const router = express.Router()
 
 var app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 app.use(router)
 
 router.get("/get", function(req, res){
-    res.send("hola get")
+    console.log(req.headers)
+    res.header({
+        "custom-header": "Nuestro servidor propio"
+    })
+    console.log(req.body)
+    res.status(201).send([{
+        error: "none", body: "Añadido correctamente"
+    }])
 })
 
 router.post("/post", function(req, res){
