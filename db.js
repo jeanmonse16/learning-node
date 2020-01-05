d9756dd06268d0a16f387df3e72593daa80ee157
Jeanpier Monserrate <monserrateluisje@gmail.com>
2020-01-05 15:55:57 -0400

    Update db.js

--- db.js
+++ db.js
@@ -3,8 +3,6 @@ const db = require("mongoose")
 db.Promise = global.Promise
 
 async function connect(url){
-    //mongodb+srv://platzi:5qH0KAe1D6uXHEMv@curso-platzi-mongodb-gxkjo.mongodb.net/test?retryWrites=true&w=majority
-    //la conexión a la base de datos cloud se resolvera con las promesas nativas de nodejs, en caso de dar error nos lo arrojara
   db.connect(url, {
     useUnifiedTopology: true,
     useNewUrlParser: true
